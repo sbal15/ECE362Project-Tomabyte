a5a85586fb425788f8b518b124590f31fd63177b
suesue3121 <sue121303@gmail.com>
2025-11-03 11:46:32 -0500

    added buzzer file
    buzzer

---
+++ src/buzzer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+static int duty_cycle = 0;
+static int dir = 0;
+static int color = 0;
+
+//declare functions here
+
+
+
+//main to configure microcontroller
+int main (){
+
+}
